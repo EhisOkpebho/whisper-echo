0acb33847cd23611f52888a28b9b2e933a7dd181
EhisOkpebho <ehis.okp@gmail.com>
2025-02-14 15:02:18 +0100

    feat(api): add transcription endpoint using whisper.cpp and update requirements

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
-from fastapi import FastAPI
+import os
+from contextlib import asynccontextmanager
+from tempfile import NamedTemporaryFile
 
-app = FastAPI()
+from fastapi import FastAPI, UploadFile, File, Query, HTTPException
+from fastapi.responses import JSONResponse
+
+from utils import convert_to_wav, transcribe
+
+temp_audio_path = None
+temp_wav_path = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    for file_path in [temp_audio_path, temp_wav_path]:
+        if file_path and os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"Deleted temporary file: {file_path}")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+@app.post("/transcribe/file")
+async def transcribe_file(file: UploadFile = File(...), model: str = Query(...)):
+    global temp_audio_path, temp_wav_path
+
+    with NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
+        temp_audio_path = temp_audio_file.name
+        temp_audio_file.write(file.file.read())
+
+    with NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav_file:
+        temp_wav_path = temp_wav_file.name
+
+    convert_to_wav(temp_audio_path, temp_wav_path)
+
+    try:
+        transcription_result = transcribe(temp_wav_path, model)
+        return JSONResponse(
+            status_code=200,
+            content={"transcription": transcription_result}
+        )
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
---
+++ utils.py
@@ -0,0 +1,26 @@
+import os.path
+import subprocess
+
+import imageio_ffmpeg as ffmpeg
+
+from config import WHISPER_PATH, WHISPER_MODEL_PATH
+
+def is_model_available(model: str):
+    return os.path.exists(f"{WHISPER_MODEL_PATH}{model}.bin")
+
+def convert_to_wav(input_path: str, output_path: str):
+    ffmpeg_cmd = [
+        ffmpeg.get_ffmpeg_exe(), "-i", input_path,
+        "-ar", "16000", "-ac", "1", output_path, "-y"
+    ]
+    subprocess.run(ffmpeg_cmd, check=True)
+
+def transcribe(file_path: str, model: str):
+    if not is_model_available(model):
+        raise FileNotFoundError(
+            f"The model '{model}' does not exist or is not yet imported. Please try to pull it first.")
+
+    return subprocess.run(
+        [WHISPER_PATH, "-m", f"{WHISPER_MODEL_PATH}{model}.bin", "-f", file_path],
+        capture_output=True, text=True, check=True
+    ).stdout
